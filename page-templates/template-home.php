9389e2ae74499898d41b5202349a80a5ff8b58cf
Mahmudul Hasan <salimsrj@gmail.com>
2022-11-09 21:54:40 +0600

    Home Desktop design has been converted

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -184,5 +184,23 @@ $container = get_theme_mod( 'understrap_container_type' );
     </div>
 </section>
 
+<section class="contact"  style="background: url(<?php echo get_stylesheet_directory_uri();?>/images/contact-bg.png); background-size: cover; background-position: center center;">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="content-box">
+                    <div>
+                        <h2 class="title">Cura helps you achieve your goals.</h2>
+                        <h3 class="sub-title">Want to see how?</h3>
+                        <div class="button-container">
+                            <a href="#" class="btn btn-contact">CONTACT US</a>
+                        </div>
+                    </div>
+                </div>                
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php
 get_footer();
