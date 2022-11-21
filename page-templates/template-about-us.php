5ff2f1ee14c54ca07259ab1a672a5ad52e46f01d
Mahmudul Hasan <salimsrj@gmail.com>
2022-11-21 17:18:27 +0600

    who we are has been designed

---
+++ page-templates/template-about-us.php
@@ -0,0 +1,207 @@
+<?php
+/**
+ * Template Name: About Us
+ *
+ * This template can be used to override the default template and sidebar setup
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<section class="inner-banner style-7"
+    style="background: url(<?php echo get_stylesheet_directory_uri();?>/images/banner-bridge.png); background-size: cover; background-position: bottom center;">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="banner-content">
+                    <div class="left-box">
+                        <h2 class="title"><span>We build bridges </span>between companies and employees.</h2>
+                    </div>
+                    <div class="right-box">
+                        <div class="content">
+                            <p>Within the Staffing Industry there are no better conduit than the experts at Cura
+                                Recrutiing. We take the time to discover your wants and needs and find you exactly the
+                                candidate you’re looking for. </p>
+                        </div>
+                        <a href="#" class="btn btn-green">Our services</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="about-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="about-content">
+                    <div class="left-box">
+                        <div class="left-content-box h-100">
+                            <div class="title-box">
+                                <h2 class="title">We are better<span>TOGETHER</span></h2>
+                            </div>
+                            <div class="text-bottom align-self-end">
+                                <p>“We focus on finding the greatest staff available, they are the ones who help you
+                                    achieve success. “</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right-box">
+                        <div class="content-box">
+                            <article>
+                                <h3>Our story</h3>
+                                <p>After years of building a solid reputation in the Staffing Industry as a recruiter
+                                    himself, our CEO realized there was massive potential in assisting staffing agencies
+                                    with hard-to-find niche positions. Building a personal connection between agencies
+                                    and those they employ became a passion, and Cura Recruiting was born.</p>
+                            </article>
+                            <article>
+                                <h3>Our mission</h3>
+                                <p>We aim to bring Emotional Intelligence back into the recruiting industry through
+                                    one-on-one interactions, policies, and methodologies. You’re not just a number when
+                                    you work with us, but a person with needs and goals - we are in this together!</p>
+                            </article>
+                            <article>
+                                <h3>Our values</h3>
+                                <p>Pursuit of Excellence. We know how important it is to do the right thing. There are
+                                    no shortcuts when it comes to sourcing and finding the ideal person for a position.
+                                    We honor everyone we work with by actively listening to their needs and keeping
+                                    communication channels open and honest. By focusing on these values, we pursue
+                                    excellence and celebrate your successes!</p>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="our-team">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="content-box">
+                    <h2 class="title">Meet our <span>professional</span> team</h2>
+                    <div class="content">
+                        <p>For decades, our team cultivated and immersed themselves in understanding the requirements
+                            and unique aspects of different trades and industries so we can help you find the “best of
+                            the best” to employ for yourselves!</p>
+                    </div>
+                </div>
+
+                <div class="team-members">
+                    <div class="member">
+                        <div class="profile-pic">
+                            <img src="<?php echo get_stylesheet_directory_uri();?>/images/profile.jpg" alt="profile">
+                        </div>
+                        <h3 class="name">Full Name</h3>
+                        <span class="designation">Founder | Director</span>
+                    </div>
+
+                    <div class="member">
+                        <div class="profile-pic">
+                            <img src="<?php echo get_stylesheet_directory_uri();?>/images/profile.jpg" alt="profile">
+                        </div>
+                        <h3 class="name">Full Name</h3>
+                        <span class="designation">Founder | Director</span>
+                    </div>
+
+                    <div class="member">
+                        <div class="profile-pic">
+                            <img src="<?php echo get_stylesheet_directory_uri();?>/images/profile.jpg" alt="profile">
+                        </div>
+                        <h3 class="name">Full Name</h3>
+                        <span class="designation">Founder | Director</span>
+                    </div>
+
+                    <div class="member">
+                        <div class="profile-pic">
+                            <img src="<?php echo get_stylesheet_directory_uri();?>/images/profile.jpg" alt="profile">
+                        </div>
+                        <h3 class="name">Full Name</h3>
+                        <span class="designation">Founder | Director</span>
+                    </div>
+
+                    <div class="member">
+                        <div class="profile-pic">
+                            <img src="<?php echo get_stylesheet_directory_uri();?>/images/profile.jpg" alt="profile">
+                        </div>
+                        <h3 class="name">Full Name</h3>
+                        <span class="designation">Founder | Director</span>
+                    </div>
+
+                    <div class="member">
+                        <div class="profile-pic">
+                            <img src="<?php echo get_stylesheet_directory_uri();?>/images/profile.jpg" alt="profile">
+                        </div>
+                        <h3 class="name">Full Name</h3>
+                        <span class="designation">Founder | Director</span>
+                    </div>
+
+                    <div class="member">
+                        <div class="profile-pic">
+                            <img src="<?php echo get_stylesheet_directory_uri();?>/images/profile.jpg" alt="profile">
+                        </div>
+                        <h3 class="name">Full Name</h3>
+                        <span class="designation">Founder | Director</span>
+                    </div>
+
+                    <div class="member">
+                        <div class="profile-pic">
+                            <img src="<?php echo get_stylesheet_directory_uri();?>/images/profile.jpg" alt="profile">
+                        </div>
+                        <h3 class="name">Full Name</h3>
+                        <span class="designation">Founder | Director</span>
+                    </div>
+                    <div class="background"
+                        style="background: url(<?php echo get_stylesheet_directory_uri();?>/images/team-bg-2.png); background-size: cover; background-position: top center;">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="connect-with-team">
+                    <div class="content-box">
+                        <div>
+                            <h2 class="team-title"><span>Have a question?</span> Our team is happy to help!</h2>
+                            <div class="content">
+                                <p>When Frequently Asked Questions aren’t enough, know that we are 100% committed to
+                                    answering all your most pressing concerns. You can call, email, or direct message us
+                                    at any time, and we will always get back to you promptly.</p>
+                            </div>
+                            <div class="buttons">
+                                <ul>
+                                    <li><a href="#" class="btn btn-blue-green">Let’s talk</a></li>
+                                    <li><a href="#" class="btn btn-blue">Our services</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="image-box">
+                        <div class="image-container">
+                            <img src="<?php echo get_stylesheet_directory_uri();?>/images/call-center.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<?php
+get_footer();
